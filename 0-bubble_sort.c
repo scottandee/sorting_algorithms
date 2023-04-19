b3426ab095c4158a4c40030240bc6a56dcd7d46b
Andee <andeeolayinkascott@gmail.com>
2023-04-19 20:05:27 +0000

    header file added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+  * bubble_sort - sorts an array of integers in 
+  * ascending order using the Bubble sort algorithm
+  * @array - this contains the numbers that are to be sorted
+  * @size - this contains the size of the array that is to be sorted
+  */
+
+void bubble_sort(int *array, size_t size)
+{
+
+}
+
