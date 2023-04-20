e11bd133ca506aff63ee990745d54c836b34b308
Andee <andeeolayinkascott@gmail.com>
2023-04-20 06:56:35 +0000

    Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,36 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	int swaps, buffer;
+	size_t i;
 
+	while (1)
+	{
+		swaps = 0;
+		
+		for (i = 0; i < size; i++)
+		{
+			if (i == size - 1)
+			{
+				continue;
+			}
+			if(array[i] > array[i + 1])
+			{
+				buffer = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = buffer;
+				swaps++;
+				print_array(array, size);
+			}
+			else
+			{
+				continue;
+			}
+		}
+
+		if (swaps == 0)
+		{
+			break;
+		}
+	}
 }
-
