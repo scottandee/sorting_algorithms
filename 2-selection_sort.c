515ba1ab9e0b2701b5c3209205d9ec5005ee88c3
Andee <andeeolayinkascott@gmail.com>
2023-04-21 18:12:08 +0000

    Bug fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,11 +10,12 @@
 void selection_sort(int *array, size_t size)
 {
 	int buffer;
-	size_t i, j, min_idx;
+	size_t i, j, min_idx, code;
 
 	for (i = 0; i < size; i++)
 	{
 		buffer = array[i];
+		code = 0;
 
 		for (j = i + 1; j < size; j++)
 		{
@@ -22,10 +23,14 @@ void selection_sort(int *array, size_t size)
 			{
 				buffer = array[j];
 				min_idx = j;
+				code = 1;
 			}
 		}
-		array[min_idx] = array[i];
-		array[i] = buffer;
-		print_array(array, size);
+		if (code == 1)
+		{
+			array[min_idx] = array[i];
+			array[i] = buffer;
+			print_array(array, size);
+		}
 	}
 }
